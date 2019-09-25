a6d2cf3ebdf6595be79e52d5cb6a825add89c16b
neimad971 <damientalbot26@gmail.com>
2019-09-25 22:12:27 +0200

    adds association between User and Task(aka Action)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_183440) do
+ActiveRecord::Schema.define(version: 2019_09_25_195353) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_09_24_183440) do
     t.text "label"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
