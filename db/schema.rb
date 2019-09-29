a57e3dd7d0c9b4dd45221ce46ade35fb613d2b56
neimad971 <damientalbot26@gmail.com>
2019-09-29 09:17:58 +0200

    sets pg for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_25_195353) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "content"
     t.boolean "completed"
