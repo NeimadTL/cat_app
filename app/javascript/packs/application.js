1295b5419a56705f80f41eaf338ea4d379211d70
neimad971 <damientalbot26@gmail.com>
2019-09-29 10:29:56 +0200

    solves issue related to JS not working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,7 +16,5 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+import 'bootstrap'
+import './src/application.scss'
